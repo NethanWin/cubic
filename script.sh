38cfd9822fe2bc98d435322d181f23cb090fd76a
NethanWin <nethan.win@protonmail.com>
2025-12-16 23:30:33 +0200

    added chroot guide

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sp -S squashfs-tools
+sudo pacman -S squashfs-tools
 
 mkdir -p ubuntu-iso-work/{iso,edit,squashfs}
 cd ubuntu-iso-work
@@ -10,4 +10,15 @@ cp .iso .
 
 sudo mount -o loop .iso iso
 
-sudo unsqu
+sudo unsquashfs -d squashfs iso/casper/filesystem.squashfs
+
+sudo cp -a squashfs tmp-fs
+
+sudo mount --bind /dev tmp-fs/dev
+sudo mount --bind /run tmp-fs/run
+sudo mount -t proc /proc tmp-fs/proc
+sudo mount -t sysfs /sys tmp-fs/sys
+sudo mount -t devpts /dev/pts tmp-fs/dev/pts
+sudo cp /etc/resolv.conf tmp-fs/etc/resolv.conf
+
+sudo chroot tmp-fs /bin/bash
