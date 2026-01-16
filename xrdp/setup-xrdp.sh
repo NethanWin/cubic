7f67072db28ed5a49997dba6d4911d9be6b1884d
NethanWin <nethan.win@protonmail.com>
2026-01-16 19:19:42 +0200

    xrdp 4

--- xrdp/setup-xrdp.sh
+++ xrdp/setup-xrdp.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+sudo apt install openssh-server git xrdp -y
 
 sudo mkdir -p /etc/polkit-1/localauthority/90-mandatory.d
 sudo cp 99-xrdp.pkla /etc/polkit-1/localauthority/90-mandatory.d/
+sudo cat /etc/polkit-1/localauthority/90-mandatory.d/99-xrdp.pkla
 
 sudo cp startwm.sh /etc/xrdp/startwm.sh
+sudo cat /etc/xrdp/startwm.sh
 
 cp .xsession $HOME/.xsession
+cat $HOME/.xsession
 
 sudo systemctl restart xrdp xrdp-sesman
